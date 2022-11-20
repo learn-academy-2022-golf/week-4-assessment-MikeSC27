dfd02f3f6c523bfb7a10c19d8bff489fa60dd2e9
MikeSC27 <mcruzen89@gmail.com>
2022-11-20 11:30:57 -0800

    week 4 assessment complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,7 +1,20 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
+// Test Driven Development
+// describe("funcName", () => {
+//   it("what funcName will do", () => {
+//     const var1 = ["1"]
+//     const var1Output = ["2"]
+//     const var2 = ["2"]
+//     const var2Output = ["4"]
+//     expect(funcName(var1)).toEqual(var1Output)
+//     expect(funcName(var2)).toEqual(var2Output)
+//   })
+// })
+
+
 // Please read all questions thoroughly
-// Pseudo coding is REQUIRED
+// Pseudo coding is REQUIRED (really, really is it. Can we just talk it out maybe?)
 // If you get stuck, please leave comments to help us understand your thought process
 
 // Use test driven development to complete the following questions
@@ -16,18 +29,87 @@
 // a) Create a test with an expect statement using the variable provided. 
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
+
+// Test Driven Development
+// describe("randomArray", () => {
+//   it("that takes in an array, removes the first item from the array and shuffles the remaining content", () => {
+//     const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+//     const colors1Output = [ 'yellow', 'pink', 'blue', 'green' ]
+//     const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+//     const colors2Output = [ 'periwinkle', 'indigo', 'ochre', 'saffron', 'aquamarine' ]
+//     expect(randomArray(colors1)).toEqual(expect.arrayContaining(colors1Output))
+//     expect(randomArray(colors2)).toEqual(expect.arrayContaining(colors2Output))
+//   })
+// })
+
+    // Jest Test RTN:
+    // ReferenceError: randomArray is not defined
+    // Test Suites: 1 failed, 1 total
+    // Tests:       1 failed, 1 total
+
 const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
 const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
-
 // b) Create the function that makes the test pass.
+
+    // Pseudocode:
+        //Paramenter will be an array 
+        //Argument: const colors1 and const colors2
+        //Create a function named randomArray 
+        //Utilize .shift() to remove the first item from the array and a for loop with a random Math.floor(Math.random()) function to randomize the remaining elements by moving placeHolder and swapping spots with another element in the array.
+        //Return the array, or else you'll be undefined...
+
+
+const randomArray = (array) => {
+      array.shift();
+      for(let i = 0; i < array.length; i++) {
+        let placeHolder = array[i]
+        let randomIndex = Math.floor(Math.random() * array.length);
+        array[i] = array[randomIndex];
+        array[randomIndex] = placeHolder
+      }
+      return array
+  }
+  console.log(randomArray(colors1))
+  console.log(randomArray(colors2))
+                // just double checking .shift() below
+                // colors1.shift()
+                // console.log(colors1)
+
+    // Jest Test Return PASS  
+    // Test Suites: 1 passed, 1 total
+    // Tests:       1 passed, 1 total
+    // Snapshots:   0 total
+    // Time:        0.197 s, estimated 1 s
+    // Ran all test suites.
+    // ✨  Done in 0.99s.
+        
+
+
 
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+// Test Driven Development
+// describe("totalTally", () => {
+//   it("takes in an object that contains up votes and down votes and returns the end tally", () => {
+//     const votes1 = {upVotes: 13, downVotes: 2}
+//     const votes1Output = 11
+//     const votes2 = {upVotes: 2, downVotes: 33}
+//     const votes2Output = -31
+//     expect(totalTally(votes1)).toEqual(votes1Output)
+//     expect(totalTally(votes2)).toEqual(votes2Output)
+//   })
+// })
+
+    // Jest Test RTN:
+    // ReferenceError: totalTally is not defined
+    // Test Suites: 1 failed, 1 total
+    // Tests:       1 failed, 1 total
 
 const votes1 = {upVotes: 13, downVotes: 2}
 // Expected output: 11
@@ -37,10 +119,52 @@ const votes2 = {upVotes: 2, downVotes: 33}
 
 // b) Create the function that makes the test pass.
 
+    // Pseudocode:
+        //Paramenter will be an object 
+        //Argument: const votes1 and const votes2
+        //Create a function named totalTally that takes in an object that contains up votes and down votes and returns the total tally of votes
+        //Utilize Object.values() to get the number values then .reduce call back function for the return value to subtract last value. 
+        //Return the total, or else you'll be undefined...
+
+
+
+const totalTally = (object) => Object.values(object).reduce((accumulater, value) => {
+    return accumulater - value;
+})
+
+console.log(totalTally(votes1))
+console.log(totalTally(votes2))
+            // const values = Object.values(votes1)
+            // console.log(values)
+            // const totalTally = Object.value(obj) <- this did not work
+
+    // Jest Test Return PASS  
+    // Test Suites: 1 passed, 1 total
+    // Tests:       1 passed, 1 total
+    // Snapshots:   0 total
+    // Time:        0.229 s, estimated 1 s
+    // Ran all test suites.
+    // ✨  Done in 1.16s.
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
+
+// Test Driven Development
+// describe("arrayGlue", () => {
+//   it("takes in two arrays as arguments and returns one array with no duplicate values", () => {
+//     const dataArray1 = ["array", "object", "number", "string", "Boolean"]
+//     const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
+//     const combinedDataArrayOutput = ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+//     expect(arrayGlue(dataArray1, dataArray2)).toEqual(combinedDataArrayOutput)
+    
+//   })
+// })
+
+    // Jest Test RTN:
+    // ReferenceError: arrayGlue is not defined
+    // Test Suites: 1 failed, 1 total
+    // Tests:       1 failed, 1 total
 
 const dataArray1 = ["array", "object", "number", "string", "Boolean"]
 const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
@@ -48,3 +172,27 @@ const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
 
 
 // b) Create the function that makes the test pass.
+
+    // Pseudocode:
+        //Paramenter will be two arrays 
+        //Argument: const dataArray1 and const dataArray2
+        //Create a function named arrayGlue 
+        //Utilize .concat to combine the two arrays and filter out any duplicates then returns one array. 
+        //Utilizing spread operator syntax (for much needed bonus points ;) this iteralble protocal iterates all elements in both arrays and with the new SetConstructor that builds a new array3 without duplicates.
+        //Return the array, or else you'll be undefined...
+
+const arrayGlue = (array1, array2) => {
+    (dataArray1).concat(dataArray2)
+    array3 = [...new Set([...dataArray1,...dataArray2])]
+    return array3
+}
+
+console.log(arrayGlue(dataArray1, dataArray2))
+
+    // Jest Test Return PASS
+    // Test Suites: 1 passed, 1 total
+    // Tests:       1 passed, 1 total
+    // Snapshots:   0 total
+    // Time:        0.201 s, estimated 1 s
+    // Ran all test suites.
+    // ✨  Done in 0.90s.
--- interview-questions.md
+++ interview-questions.md
@@ -8,42 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is object-oriented programming? How is it different than functional programming?
 
-Your answer:
+Your answer: Object-oriented programming is a langueage such as Ruby that emphasizes on data such as classes and objects where objects are instances of the their class. I will have to get back to you with a little more research on the specifics of functional programming other than a langueage such as Javascript based on functional procedure. 
 
-Researched answer:
+Researched answer: The Ruby object oriented programming is a good way to organize your code in classes or a hash with objects that helps maintain large applications that way you can make changes to without really affecting the entire program especially if there was an error or bug it may stay compartmentalized.  Functional programming is not centered around classes and objects but instead predefined first class functions.  The first class functions in Javascript are required for functional programming that can be passed around same as any value.  This leads to the built in methods that allow you to just call or invoke a function without having to type or build the scripting for the function and this can lead to less coding required.  Anothers words a main difference between the two paradigms are that object oriented programming can be compartmentalized. 
+
 
 2. What is the difference between a Float and an Integer in Ruby?
 
-Your answer:
+Your answer: In Ruby, an Integer is always a whole number and Float, not be confused with decimal is a number with in decimal form that includes remainders and if you expect a Float number you must start with a Float number such as 7.0. 
 
-Researched answer:
+Researched answer: In Ruby any number typed without a decimal will be considered an integer or whole number such as 7.  A float is written in decimal form such as 7.0 but is not as precise in its calculations in Ruby.  You would need to use BigDecimal to have a more precise decimal answer.
+
 
 3. Ruby has an implicit return. What does that mean?
 
-Your answer:
+Your answer: The implicit return in Ruby means you do not have to type or command the return of the last line of code or method.
 
-Researched answer:
+Researched answer:  Within Ruby you do not have to type a return statement, it is going to return the last line of code evaluated. This is a simple and more implied type of coding that Ruby likes to implement.  
 
 4. What is a block in Ruby?
 
-Your answer:
+Your answer: In Ruby blocks are anonomous functions that are surrounded in scope by do and end or sometimes curly brackets. 
 
-Researched answer:
+Researched answer: Since we are talking about Ruby and we know there are technically or subjectivly no functions but only methods, well blocks are methods that dont really belong to an object.  They are anonomous or nameless bits of code that can accept arguments in these | |, (as I hold up my arms in a double pipe form, indicating I have kicked this interview right through the uprights for a field goal;). Blocks can be called within the method that it was passed to inside a do and end sytax statement. 
 
 1. How do you extract a value in a Ruby hash?
 
-Your answer:
+Your answer: In order to extract a value from a key value pair Ruby hash you would need to type puts hash_name.key of the value you want extracted.
 
-Researched answer:
+Researched answer: If you have a hash and you want to extract from it you can use the the extract method with a bang .extract!(:key).values.
 
 ## Looking Ahead: Terms for Next Week
 
-1. Class Inheritance:
+1. Class Inheritance: Helps Ruby create a better version of a class.  This is done by passing down methods from parent to child classes. 
 
-2. RSpec:
+2. RSpec: This is a testing framework for Ruby that focuses on behaviors of the code. 
 
-3. CRUD:
+3. CRUD: Create Read Update Delete the four major operations we can do on data utilizing Ruby. 
 
-4. Test-driven development:
+4. Test-driven development: A testing framework that allows you to test your code scripts seperately. 
 
-5. HTTP:
+5. HTTP: This is a client library you can request by making a GET or POST?  Not to clear on this yet though. Seems like it's from Ruby on Rails framework. 
